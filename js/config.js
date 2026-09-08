27bbcd97a2a36271059c14ea203aeaaf8b654ce4
aaybeeelectricals <aaybeeelectricals1@gmail.com>
2026-09-08 21:26:09 +0530

    Delete js directory

--- js/config.js
+++
@@ -1,4 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-export const SUPABASE_URL="https://xiufnzlnuiajhswtcgsy.supabase.co";
-export const SUPABASE_ANON_KEY="sb_publishable_Y-g3pZWUHVcZFpoKtZ3Lpw_RJzPXLCI";
-export const supabase=createClient(SUPABASE_URL,SUPABASE_ANON_KEY);
